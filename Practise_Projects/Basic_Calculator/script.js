5443d396fa37f231ea845f0d76b3ac6f8e5b6158
Desai Reddy Leela Madhava Reddy <104156901+leela7032@users.noreply.github.com>
2026-04-21 01:10:56 +0530

    All my files

---
+++ Practise_Projects/Basic_Calculator/script.js
@@ -0,0 +1,20 @@
+const display=document.getElementById('display');
+function addToDisplay(input){
+    display.value += input;
+}
+function calculate(){
+    try{
+        display.value=eval(display.value);
+
+    }
+    catch (error){
+            display.value="Typing Wrong";
+        }
+    }
+    function clear1(){
+        display.value="";
+    }
+
+
+
+  
